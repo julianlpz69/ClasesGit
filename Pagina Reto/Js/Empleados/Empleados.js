a6bd3e821a5828feea5827be36bc13a22f2f513f
Nicolas sarchi <nicolassarchipico@gmail.com>
2023-09-28 13:33:39 -0500

    feat: :sparkles: CRUD empleado

--- Pagina Reto/Js/Empleados/Empleados.js
+++ Pagina Reto/Js/Empleados/Empleados.js
@@ -4,8 +4,9 @@ import {
   eliminarEmpleado,
   URL,
   modificarEmpleado,
-  agregarDireccion
-  
+  getDeptos,
+  getPaises,
+  getCiudades,
 } from "./empleadoRequest.js";
 
 var nombreCookie = "userName";
@@ -18,52 +19,28 @@ nombreUser.textContent = getCookieValue(nombreCookie);
 const formEmpleados = document.getElementById("agregar-empleado"),
   tablaEmpleados = document.getElementById("tabla-Empleados");
 
-let EmpleadoEditando = null;
-
-formEmpleados.addEventListener("submit", (event) => {
+formEmpleados.addEventListener("submit", async (event) => {
   event.preventDefault();
-
-  const id = document.getElementById("id").value,
-    nombre = document.getElementById("nombre").value,
-    apellido = document.getElementById("apellido").value,
-    tipoDoc = document.getElementById("tipoDoc").value,
-    cargoEmpleado = document.getElementById("cargoEmpleado").value,
-    tipoVia = document.getElementById("tipoVia").value,
-    ciudad = document.getElementById("ciudad").value,
-    calle = document.getElementById("calle").value,
-    numeroDir = document.getElementById("numeroDir").value,
-    carrera = document.getElementById("carrera").value,
-    complemento = document.getElementById("complemento").value,
-    telefono = document.getElementById("telefono").value;
-  const datosEmpleado = {
-    id,
-    nombre,
-    apellido,
-    tipoDoc,
-    cargoEmpleado,
-    telefono,
+  const direccion = {
+    DireccionCalle: document.getElementById("calle").value,
+    DireccionNumero: document.getElementById("numeroDir").value,
+    DireccionCarrera: document.getElementById("carrera").value,
+    DireccionTipoVia: document.getElementById("tipoVia").value,
+    DireccionIdCiudadFk: document.getElementById("ciudad").value,
+    EmpleadoNombre: document.getElementById("nombre").value,
+    EmpleadoApellido: document.getElementById("apellido").value,
+    EmpleadoCedula: document.getElementById("id").value,
+    EmpleadoTelefono: document.getElementById("telefono").value,
+    EmpleadoIdTipoDocumentoFk: document.getElementById("tipoDoc").value,
+    EmpleadoIdCargoEmpleadoFk: document.getElementById("cargoEmpleado").value,
   };
-  const datosDir = {
-    tipoVia,
-    ciudad,
-    calle,
-    numeroDir,
-    carrera,
-    complemento,
-  };
-  
-  agregarEmpleado(datosEmpleado);
 
-  // mostrarEmpleados();
-  // actualizarSelectEmpleado();
-  // actualizarListaPuntos();
-  formEmpleados.reset();
+  await agregarEmpleado(direccion);
+  window.location.reload();
 });
-
-
 const Empleados = await getEmpleados();
-console.log(Empleados)
-mostrarEmpleados(Empleados)
+console.log(Empleados);
+mostrarEmpleados(Empleados);
 function mostrarEmpleados(EmpleadosAMostrar) {
   tablaEmpleados.innerHTML = "";
 
@@ -76,64 +53,158 @@ function mostrarEmpleados(EmpleadosAMostrar) {
         <td>${Empleado.apellido}</td>
         <td>${Empleado.telefono}</td>
         <td><button class="btn btn-danger btn-eliminar" data-id = "${Empleado.id}">Eliminar</button></td>
-        <td><button class="btn btn-warning btn-editar" data-id ="${Empleado.id}">Editar</button></td>
+        <td><button class="btn btn-warning btn-editar" data-id ="${Empleado.id}" data-bs-toggle="modal" data-bs-target="#exampleModal">Editar</button></td>
         `;
     tablaEmpleados.appendChild(nuevafila);
   });
 }
+const selectPais = document.getElementById("pais"),
+  selectDepto = document.getElementById("departamento"),
+  selectCiudad = document.getElementById("ciudad"),
+  selectPaisMod = document.getElementById("paisEdit"),
+  selectDeptoMod = document.getElementById("departamentoEdit"),
+  selectCiudadMod = document.getElementById("ciudadEdit");
+async function cargarSelect(
+  selectElement,
+  data,
+  idField,
+  nameField,
+  onChangeCallback
+) {
+  try {
+    data.forEach((item) => {
+      let option = `
+        <option value="${item[idField]}" data-id="${item[idField]}">${item[nameField]}</option>
+      `;
+      selectElement.innerHTML += option;
+    });
 
-document.addEventListener("click", (e) => {
+    if (onChangeCallback) {
+      selectElement.addEventListener("change", (e) => {
+        const selectedId = e.target.value;
+        onChangeCallback(selectedId);
+      });
+    }
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+async function cargarPaises() {
+  const Paises = await getPaises();
+  cargarSelect(selectPais, Paises, "id", "nombre", cargarDeptos);
+  cargarSelect(selectPaisMod, Paises, "id", "nombre", cargarDeptos);
+}
+
+async function cargarDeptos(paisId) {
+  const deptos = await getDeptos(paisId);
+  cargarSelect(
+    selectDepto,
+    deptos.departamentos,
+    "id",
+    "nombre",
+    cargarCiudades
+  );
+  cargarSelect(
+    selectDeptoMod,
+    deptos.departamentos,
+    "id",
+    "nombre",
+    cargarCiudades
+  );
+}
+
+async function cargarCiudades(deptoId) {
+  const Ciudades = await getCiudades(deptoId);
+  cargarSelect(selectCiudad, Ciudades.ciudades, "id", "nombre");
+  cargarSelect(selectCiudadMod, Ciudades.ciudades, "id", "nombre");
+}
+
+cargarPaises();
+
+document.addEventListener("click",async (e) => {
   if (e.target.classList.contains("btn-eliminar")) {
     const empleadoId = e.target.dataset.id;
     console.log(empleadoId);
-    eliminarEmpleado(empleadoId);
+    await eliminarEmpleado(empleadoId);
+    window.location.reload();
   }
 });
 
-function borrarEmpleado(EmpleadoId) {
-  const indice = Empleados.findIndex((Empleado) => Empleado.id === EmpleadoId);
-  if (indice !== -1) {
-    Empleados.splice(indice, 1);
-    mostrarEmpleados();
+document.addEventListener("click", async (e) => {
+  if (e.target.classList.contains("btn-editar")) {
+    const empleadoId = e.target.dataset.id;
+    document.getElementById("guardarCambios").setAttribute("data-id", empleadoId);
+    try {
+      const response = await fetch(`${URL}/Empleado/${empleadoId}`);
+      const empleado = await response.json();
+
+      const formModificarempleado = document.getElementById("editar-empleado");
+      formModificarempleado.setAttribute("data-id", empleadoId);
+      document.getElementById("calleEdit").value = empleado.direccion.calle,
+        document.getElementById("numeroDirEdit").value = empleado.direccion.numero,
+        document.getElementById("carreraEdit").value = empleado.direccion.carrera,
+        document.getElementById("tipoViaEdit").value = empleado.direccion.tipoVia,
+        document.getElementById("ciudadEdit").value = empleado.direccion.idCiudadFk,
+        document.getElementById("nombreEdit").value = empleado.nombre,
+        document.getElementById("apellidoEdit").value = empleado.apellido,
+        document.getElementById("idEdit").value  = empleado.cedula,
+        document.getElementById("telefonoEdit").value = empleado.telefono,
+        document.getElementById("tipoDocEdit").value = empleado.idTipoDocumentoFk,
+        document.getElementById("cargoEmpleadoEdit").value = empleado.idCargoEmpleadoFk;
+    } catch (error) {
+      console.error(error);
+    }
   }
-}
+});
 
-function editarEmpleadoForm(EmpleadoId) {
-  const Empleado = Empleados.find((Empleado) => Empleado.id === EmpleadoId);
-
-  if (Empleado) {
-    id = document.getElementById("id").value = Empleado.id;
-    nombre = document.getElementById("nombre").value = Empleado.nombre;
-    tipoDoc = document.getElementById("tipoDoc").value = Empleado.tipoDoc;
-    telefono = document.getElementById("telefono").value = Empleado.telefono;
-    apellido = document.getElementById("apellido").value = Empleado.apellido;
-    cargo = document.getElementById("cargoEmpleado").value =
-      Empleado.cargoEmpleado;
-    ciudad = document.getElementById("ciudad").value = Empleado.ciudad;
-    calle = document.getElementById("calle").value = Empleado.calle;
-    numeroDir = document.getElementById("numeroDir").value = Empleado.numeroDir;
-    carrera = document.getElementById("carrera").value = Empleado.carrera;
-    complemento = document.getElementById("complemento").value =
-      Empleado.complemento;
-    tipoVia = document.getElementById("tipoVia").value = Empleado.tipoVia;
-
-    EmpleadoEditando = Empleado;
-  }
-}
-
-function editarEmpleado(EmpleadoEditando) {
-  const indice = Empleados.findIndex(
-    (Empleado) => Empleado.id === EmpleadoEditando.id
-  );
-
-  if (indice !== -1) {
-    Empleados[indice] = EmpleadoEditando;
-  }
-
-  mostrarEmpleados();
-  actualizarSelectEmpleado();
-  actualizarListaPuntos();
-}
+document
+  .getElementById("guardarCambios")
+  .addEventListener("click", async (e) => {
+    const empleadoID = e.target.dataset.id;
+    console.log(empleadoID);
+    const nombre = document.getElementById("nombreEdit").value;
+    const apellido = document.getElementById("apellidoEdit").value;
+    const cedula = document.getElementById("idEdit").value;
+    const telefono = document.getElementById("telefonoEdit").value;
+    const direccionTipoVia = document.getElementById(
+      "tipoViaEdit"
+    ).value;
+    const direccionCalle = document.getElementById("calleEdit").value;
+    const direccionCarrera = document.getElementById(
+      "carreraEdit"
+    ).value;
+    const direccionNumero = document.getElementById(
+      "numeroDirEdit"
+    ).value;
+    const direccionIdCiudadFk = document.getElementById(
+      "ciudadEdit"
+    ).value;
+    const idCargoEmpleadoFk =
+      document.getElementById("cargoEmpleadoEdit").value;
+    const idTipoDocumentoFK =
+      document.getElementById("tipoDocEdit").value;
+console.log(idCargoEmpleadoFk);
+    const empleadoDtoActualizado = {
+      id: empleadoID,
+      nombre: nombre,
+      apellido: apellido,
+      cedula: cedula,
+      telefono: telefono,
+      direccion: {
+        tipoVia: direccionTipoVia,
+        calle: direccionCalle,
+        carrera: direccionCarrera,
+        numero: direccionNumero,
+        idCiudadFk: direccionIdCiudadFk,
+      },
+      idCargoEmpleadoFk: idCargoEmpleadoFk,
+      idTipoDocumentoFK: idTipoDocumentoFK,
+    };
+    console.log(empleadoDtoActualizado);
+    await modificarEmpleado(empleadoDtoActualizado, empleadoID);
+   window.location.reload();
+  });
 
 function buscarEmpleado() {
   const busqueda = document.getElementById("buscar").value.toLowerCase();
@@ -156,3 +227,4 @@ function buscarEmpleado() {
   mostrarEmpleados(resultadoBusqueda);
 }
 
+document.getElementById("buscar").addEventListener("keyup", buscarEmpleado);

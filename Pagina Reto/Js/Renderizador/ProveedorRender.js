acb156ffaac086626313d8c9099f7dad28b0f440
anderson cespedes <andersontrabajos2@gmail.com>
2023-09-27 21:51:56 -0500

    feat: :sparkles: agragacion de eliminacion y vistas

--- Pagina Reto/Js/Renderizador/ProveedorRender.js
+++ Pagina Reto/Js/Renderizador/ProveedorRender.js
@@ -1,10 +1,28 @@
 import * as provReq from "../Peticiones/ProveedorRequest.js";
+
 async function CrearTablaProveedores(){
     const tablageneral = document.getElementById("tabla-proveedor");
-    let data = await provReq.GetProveedorQueMasHaVendido();
+    let data = await provReq.TodosLosProveedores();
     data.forEach(element => {
-        data.in
+        let tr = tablageneral.insertRow(1);
+        let nombre = tr.insertCell(0);
+        let contacto = tr.insertCell(1);
+        let direccion = tr.insertCell(2);
+        let opc = tr.insertCell(3);
+        let dir = `Carrera ${element.direccion.carrera} Calle ${element.direccion.calle} Numero ${element.direccion.numero}`;
+        contacto.innerHTML = element.contactoProveedor
+        nombre.innerHTML = element.nombreProveedor;
+        direccion.innerHTML = dir;
+        opc.innerHTML = `<button class="btn btn-warning" id= "f${element.id}">Borrar</button>`
+        document.getElementById("f" + element.id).addEventListener("click",Eliminar);
+
     });
-    tablageneral.innerHTML = data[0].nombreProveedor;
 }
 CrearTablaProveedores();
+export function Eliminar(ev){
+    let id = parseInt( ev.target.getAttribute("id").match(/[0-9]/g).join(""));
+    provReq.EliminarProveedor(id);
+    let fila = ev.target.parentNode.parentNode;
+    let tabla = fila.parentNode;
+    tabla.removeChild(fila);
+}

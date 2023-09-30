afc09ed141a49b7e354c2b2ae37648a5f23275cc
anderson cespedes <andersontrabajos2@gmail.com>
2023-09-30 04:53:08 -0500

    fix: :bug: arreglo de elementos del html
    se repararon ciertos problemas del boton de regresar del modulo de proveedores

--- Pagina Reto/Js/Empleados/ProveedorRequest.js
+++ Pagina Reto/Js/Empleados/ProveedorRequest.js
@@ -2,113 +2,113 @@
 import { config } from "../Config/FetchConfig.js";
 
 const URL = "http://localhost:5223/Farmacia/Proveedor/";
-export async function GetProveedorQueMasHaVendido(){
-    try{
-       var datos = await fetch(`${URL}ProvMasVendio`,config());
+export async function GetProveedorQueMasHaVendido() {
+    try {
+        var datos = await fetch(`${URL}ProvMasVendio`, config());
         var json = await datos.json();
         console.log(await json);
-        return json; 
-    }catch(err){
+        return json;
+    } catch (err) {
         console.log(err);
     }
 }
-export async function GetProveedorById(id){
-    try{
-        var datos = await fetch(`${URL}Find/${id}`,config());
-         var json = await datos.json();
-         console.log(await json);
-         return json; 
-     }catch(err){
-         console.log(err);
-     }
-}
-export async function GetMedicamentosDelProveedorPorNombre(name){
-    try{
-        var datos = await fetch(`${URL}Nombre?m=${name}`,config());
+export async function GetProveedorById(id) {
+    try {
+        var datos = await fetch(`${URL}Find/${id}`, config());
         var json = await datos.json();
         console.log(await json);
-        return json;  
-    }catch(err){
-        console.log(err);
-    } 
-}
-export async function Total2023PorProveedor(){
-    try{
-        var datos = await fetch(`${URL}TotalAnualPerProv`,config());
-        var json = await datos.json();
-        console.log(await json);
-        return json;  
-    }catch(err){
+        return json;
+    } catch (err) {
         console.log(err);
     }
 }
-export async function ProveedorConMenosDe50ElementosEnStock(){
-    try{
-        var datos = await fetch(`${URL}provlessthan50med`,config());
+export async function GetMedicamentosDelProveedorPorNombre(name) {
+    try {
+        var datos = await fetch(`${URL}Nombre?m=${name}`, config());
         var json = await datos.json();
         console.log(await json);
-        return json;  
-    }catch(err){
+        return json;
+    } catch (err) {
         console.log(err);
     }
 }
-export async function TodosLosProveedores(){
-    try{
-        var datos = await fetch(`${URL}`,config());
+export async function Total2023PorProveedor() {
+    try {
+        var datos = await fetch(`${URL}TotalAnualPerProv`, config());
         var json = await datos.json();
         console.log(await json);
-        return json;  
-    }catch(err){
+        return json;
+    } catch (err) {
         console.log(err);
     }
 }
-export async function ProvQueHanSuministradoAlMenos5Medicamentos(){
-    try{
-        var datos = await fetch(`${URL}provmorethan5`,config());
+export async function ProveedorConMenosDe50ElementosEnStock() {
+    try {
+        var datos = await fetch(`${URL}provlessthan50med`, config());
         var json = await datos.json();
         console.log(await json);
-        return json;  
-    }catch(err){
+        return json;
+    } catch (err) {
         console.log(err);
     }
 }
-export async function TotalPorProveedor(){
-    try{
-        var datos = await fetch(`${URL}TotalPerProv`,config());
+export async function TodosLosProveedores() {
+    try {
+        var datos = await fetch(`${URL}`, config());
         var json = await datos.json();
         console.log(await json);
-        return json;  
-    }catch(err){
+        return json;
+    } catch (err) {
         console.log(err);
     }
 }
-export async function GuardarProveedor(body){
-    try{
-        var datos = await fetch(`${URL}GuardarProveedor`,config(body, "POST"));
+export async function ProvQueHanSuministradoAlMenos5Medicamentos() {
+    try {
+        var datos = await fetch(`${URL}provmorethan5`, config());
         var json = await datos.json();
         console.log(await json);
-        return json;  
-    }catch(err){
+        return json;
+    } catch (err) {
         console.log(err);
     }
 }
-export async function ActualizarProveedor(id, body){
-    try{
-        var datos = await fetch(`${URL}Update/${id}`,config(body, "PUT"));
+export async function TotalPorProveedor() {
+    try {
+        var datos = await fetch(`${URL}TotalPerProv`, config());
         var json = await datos.json();
         console.log(await json);
-        return json;  
-    }catch(err){
+        return json;
+    } catch (err) {
         console.log(err);
     }
 }
-export async function EliminarProveedor(id){
-    try{
-        var datos = await fetch(`${URL}Eliminar/${id}`,config(null, "DELETE"));
+export async function GuardarProveedor(body) {
+    try {
+        var datos = await fetch(`${URL}GuardarProveedor`, config(body, "POST"));
         var json = await datos.json();
         console.log(await json);
-        return json;  
-    }catch(err){
+        return json;
+    } catch (err) {
+        console.log(err);
+    }
+}
+export async function ActualizarProveedor(id, body) {
+    try {
+        var datos = await fetch(`${URL}Update/${id}`, config(body, "PUT"));
+        var json = await datos.json();
+        console.log(await json);
+        return json;
+    } catch (err) {
+        console.log(err);
+    }
+}
+export async function EliminarProveedor(id) {
+    try {
+        var datos = await fetch(`${URL}Eliminar/${id}`, config(null, "DELETE"));
+        var json = await datos.json();
+        console.log(await json);
+        return json;
+    } catch (err) {
         console.log(err);
     }
 }
--- Pagina Reto/Js/Renderizador/ProveedorRender.js
+++ Pagina Reto/Js/Renderizador/ProveedorRender.js
@@ -1,5 +1,5 @@
 import * as provReq from "../Empleados/ProveedorRequest.js";
-import { getCookieValue, PaginaRol,RefrescarToken } from "../Config/Cookies.js";
+import { getCookieValue, PaginaRol, RefrescarToken } from "../Config/Cookies.js";
 
 RefrescarToken()
 
@@ -11,16 +11,17 @@ nombreUser.textContent = getCookieValue("userName");
 const btnRegresar2 = document.getElementById("ProvvedorRegresar");
 
 const GuardarModal = document.getElementById("GuardarProv");
-const numeroRandom = function(max){
-     return Math.floor(Math.random() * max);
+const numeroRandom = function (max) {
+    return Math.floor(Math.random() * max);
 }
 
-btnRegresar2.addEventListener("click", e =>{
+btnRegresar2.addEventListener("click", e => {
     PaginaRol()
 })
+// Guardado de proveedores nuevos
 GuardarModal.addEventListener("submit", async (ev) => {
     ev.preventDefault();
-    try{
+    try {
         let datos = ev.target;
         let nombre = datos.nombre.value;
         let contacto = datos.telefono.value;
@@ -30,30 +31,31 @@ GuardarModal.addEventListener("submit", async (ev) => {
         let complemento = datos.complemento.value;
         let ciudad = datos.ciudad.value;
         let body = {
-            NombreProveedor:nombre,
-            ContactoProveedor:contacto,
-            Direccion:{
-                IdCiudadFk:ciudad,
-                Carrera:carrera,
-                Calle:calle,
-                Numero:numero,
-                complemento:complemento
+            NombreProveedor: nombre,
+            ContactoProveedor: contacto,
+            Direccion: {
+                IdCiudadFk: ciudad,
+                Carrera: carrera,
+                Calle: calle,
+                Numero: numero,
+                complemento: complemento
             }
         }
         await provReq.GuardarProveedor(body);
         alert("se ha guardado Correctamente")
         location.reload();
-    }catch(err){
+    } catch (err) {
         alert(err);
     }
 });
-async function CrearTablaProveedoresPorCantidadVendida(){
+// Render de la tabla de proveedores por cantidad vendida
+async function CrearTablaProveedoresPorCantidadVendida() {
     let contenedor = document.getElementById("diagrama_proveedor");
     let data = await provReq.Total2023PorProveedor();
-    let stonkmax = parseInt(data.sort((a,b) => parseInt(b.totalAnual) - parseInt(a.totalAnual ))[0].totalAnual) + 20;
+    let stonkmax = parseInt(data.sort((a, b) => parseInt(b.totalAnual) - parseInt(a.totalAnual))[0].totalAnual) + 20;
     data.forEach(e => {
         let totalAnual = parseInt(e.totalAnual)
-        let arre = Math.ceil( (  parseInt(totalAnual)/ stonkmax) * 100)
+        let arre = Math.ceil((parseInt(totalAnual) / stonkmax) * 100)
         let porc = arre / 2
         contenedor.innerHTML += `
         <div class= "col-md-6  " >
@@ -62,13 +64,14 @@ async function CrearTablaProveedoresPorCantidadVendida(){
                 <h6 style="color:grey;">Total: ${e.totalAnual}$</h6>
             </div>
         </div>`
-      
+
     });
 }
-async function ProveedorQueMasVendio(){
+// render para los dos proveedores destacados
+async function ProveedorQueMasVendio() {
     let contenedor = document.getElementById("vendedorNumero1");
     let dato = await provReq.GetProveedorQueMasHaVendido()
-    let datoOrdenado = dato.sort((a,b) => a-b)
+    let datoOrdenado = dato.sort((a, b) => a - b)
     contenedor.innerHTML = `
         <div class = "card bg-primary p-3 text-white">
             <h4>Nombre: ${datoOrdenado[0].nombreProveedor}</h2>
@@ -82,7 +85,8 @@ async function ProveedorQueMasVendio(){
         </div>
     `
 }
-async function CrearTablaProveedores(){
+// Render para tablas de proveedores generales
+async function CrearTablaProveedores() {
     const tablageneral = document.getElementById("tabla-proveedor");
     let data = await provReq.TodosLosProveedores();
     data.forEach(element => {
@@ -98,12 +102,13 @@ async function CrearTablaProveedores(){
         direccion.innerHTML = dir;
         opc.innerHTML = `<button class="btn btn-warning" id= "f${element.id}">Borrar</button> 
         <button class = "btn btn-success" id= "x${element.id}" data-toggle="modal" data-target="#ModalEdicion">Editar</button>`
-        document.getElementById("f" + element.id).addEventListener("click",Eliminar);
+        document.getElementById("f" + element.id).addEventListener("click", Eliminar);
         document.getElementById("x" + element.id).addEventListener("click", llenarInputsConDatos)
     });
 }
-async function llenarInputsConDatos(ev){
-    let id = parseInt( ev.target.getAttribute("id").match(/[0-9]/g).join(""));
+// llenar los inputs del formulario de actualizacion
+async function llenarInputsConDatos(ev) {
+    let id = parseInt(ev.target.getAttribute("id").match(/[0-9]/g).join(""));
     let datos = await provReq.GetProveedorById(id);
     let form = document.getElementById("updateProv");
     form.idProv.value = datos.id;
@@ -115,7 +120,8 @@ async function llenarInputsConDatos(ev){
     form.numero.value = datos.direccion.numero;
     form.complemento.value = datos.direccion.complemento;
 }
-async function StockMenos50(){
+// Render para generar solamente los proveedores con menos de 50 elementos en stock
+async function StockMenos50() {
     let datos = await provReq.ProveedorConMenosDe50ElementosEnStock();
     let contendor = document.getElementById("stockless50");
     console.log(datos)
@@ -143,24 +149,26 @@ async function StockMenos50(){
         console.log(e.nombreProveedor)
     })
 }
-export function Eliminar(ev){
-    let id = parseInt( ev.target.getAttribute("id").match(/[0-9]/g).join(""));
+// funcion para eliminar un proveedor
+export function Eliminar(ev) {
+    let id = parseInt(ev.target.getAttribute("id").match(/[0-9]/g).join(""));
     provReq.EliminarProveedor(id);
     let fila = ev.target.parentNode.parentNode;
     let tabla = fila.parentNode;
     tabla.removeChild(fila);
 }
 
-function ASD (){
+function ASD() {
     RefrescarToken()
 
     setTimeout(() => {
-      
-      }, 500);
+
+    }, 500);
 }
+// Actualizacion general de Proveedor
 document.getElementById("updateProv").addEventListener("submit", async (ev) => {
     ev.preventDefault();
-    try{
+    try {
         let datos = ev.target;
         let nombre = datos.nombre.value;
         let contacto = datos.telefono.value;
@@ -171,30 +179,30 @@ document.getElementById("updateProv").addEventListener("submit", async (ev) => {
         let ciudad = datos.ciudad.value;
         let idDireccionFk = parseInt(datos.idDireccion.value);
         let body = {
-            NombreProveedor:nombre,
-            ContactoProveedor:contacto,
-            idDireccionFk:idDireccionFk,
-            Direccion:{
-                IdCiudadFk:ciudad,
-                Carrera:carrera,
-                Calle:calle,
-                Numero:numero,
-                complemento:complemento
+            NombreProveedor: nombre,
+            ContactoProveedor: contacto,
+            idDireccionFk: idDireccionFk,
+            Direccion: {
+                IdCiudadFk: ciudad,
+                Carrera: carrera,
+                Calle: calle,
+                Numero: numero,
+                complemento: complemento
             }
         }
-        await provReq.ActualizarProveedor(parseInt(datos.idProv.value),body);
+        await provReq.ActualizarProveedor(parseInt(datos.idProv.value), body);
         alert("se ha guardado Correctamente")
         location.reload();
-    }catch(err){
+    } catch (err) {
         alert(err);
     }
 });
-
+// Render de la lista de medicamentos por nombre de proveedor
 selects.addEventListener("click", async (ev) => {
     let name = ev.target.value;
     let dato = await provReq.GetMedicamentosDelProveedorPorNombre(name);
     let meds = document.getElementById("medByName");
-    dato[0].vendidos.forEach((e,i) => {
+    dato[0].vendidos.forEach((e, i) => {
         meds.innerHTML += `<h3>${i + 1} -> ${e.nombreProducto}</h3>`;
     })
     console.log(dato);

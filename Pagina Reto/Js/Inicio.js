acce645c38ebb54b4ffb6b11f8220b482d6ba361
julianlpz69 <julianjoselpz2019@gmail.com>
2023-10-09 13:13:25 -0300

    ss

--- Pagina Reto/Js/Inicio.js
+++ Pagina Reto/Js/Inicio.js
@@ -1,9 +1,23 @@
 
 
+var logoUser = document.getElementById("logoUser");
+var nombreUser = document.getElementById("nombreUser");
+    
+    // Asignar la ruta de la imagen al atributo src del elemento img
 
+ $.ajax ({
+    url: 'https://localhost:7051/account/ultimo-user', // URL del endpoint en tu servidor
+    method: 'GET',
+    success: function(data) {
+        // Maneja los datos del usuario aquí
+        logoUser.src= data.picture;
+        nombreUser.textContent = data.userEmail
 
-import { getCookieValue } from "./Config/Cookies.js";
+        console.log(data);
+    },
+    error: function(error) {
+        // Maneja los errores si la solicitud no se completa correctamente
+        console.error(error);
+    }
+});
 
-var ss = "Mpppe"
-
-console.log(getCookieValue(ss))

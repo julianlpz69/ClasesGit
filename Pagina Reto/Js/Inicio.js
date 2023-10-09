42c05776ebca6c62510d5facb0bd871ca4e83375
julianlpz69 <julianjoselpz2019@gmail.com>
2023-10-08 22:23:54 -0300

    ss

--- Pagina Reto/Js/Inicio.js
+++ Pagina Reto/Js/Inicio.js
@@ -2,31 +2,8 @@
 
 
 
-import { getCookieValue, cokie, RefrescarToken} from "./Config/Cookies.js";
+import { getCookieValue } from "./Config/Cookies.js";
 
+var ss = "Mpppe"
 
-var nombreCookie = "userName";
-
-
-const btnRegresar = document.getElementById("InicioRegresar");
-
-
-btnRegresar.addEventListener("click", e =>{
-
-    cokie()
-    window.location.replace("../Html/Index.html");
-})
-
-
-var nombreCookie2 = "UserActivo";
-
-if(getCookieValue(nombreCookie2) == "false"){
-    window.location.replace("../Html/Pagina_Login.html");
-}
-
-const nombreUser = document.getElementById("nombreUser");
-
-nombreUser.textContent = getCookieValue(nombreCookie);
-
-
-
+console.log(getCookieValue(ss))
